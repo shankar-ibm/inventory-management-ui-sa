418303033728ae5a69be4aa5b6a7afd973247e2e
Shankar Athinarayanan <shankar@ibm.com>
2020-01-08 11:50:48 -0500

    Update code

--- client/src/components/StockItemList.jsx
+++ client/src/components/StockItemList.jsx
@@ -14,7 +14,43 @@ import Header from "../pattern-components/Header";
 import "../pattern-components/patterns.scss";
 
 class StockItemList extends Component {
-  ...
+  title = 'Stock Items';
+    subtitle = 'This is the current inventory of items';
+    columns = [
+      "name",
+      "description",
+      "stock",
+      "unitPrice",
+      "picture",
+      "manufacturer",
+    ];
+    formatters = {};
+    data = [
+      {
+        "name": "Item 1",
+        "description": "The first item",
+        "stock": 10,
+        "unitPrice": 100.0,
+        "picture": "test",
+        "manufacturer": "unknown",
+      },
+      {
+        "name": "Item 2",
+        "description": "The second item",
+        "stock": 15,
+        "unitPrice": 120.5,
+        "picture": "test1",
+        "manufacturer": "Apple",
+      },
+      {
+        "name": "Item 3",
+        "description": "The third item",
+        "stock": 20,
+        "unitPrice": 75.5,
+        "picture": "test1",
+        "manufacturer": "Sony",
+      }
+    ];
 }
 
 export default StockItemList;
